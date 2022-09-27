107a14393728c156dd5a310c6cc5b0a0db0fec87
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-09-27 15:52:17 +0300

    added animation

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 const partPair = [
-    {
-        cardId: "01",
-        image: "img/acorn.svg"
-     },
-    {
-        cardId: "02",
-        image: "img/autumn-tree.svg"
-  },   
-    {
-        cardId: "03",
-        image: "img/dry-leaf.svg"
-     },
-    {
-        cardId: "04",
-        image: "img/hedgehog.svg"
-  }, 
-    {
-        cardId: "05",
-        image: "img/pumpkin.svg"
-     },
-    {
-        cardId: "06",
-        image: "img/squirrel.svg"
-  },  
+  {
+    cardId: "01",
+    image: "img/acorn.svg"
+  },
+  {
+    cardId: "02",
+    image: "img/autumn-tree.svg"
+  },
+  {
+    cardId: "03",
+    image: "img/dry-leaf.svg"
+  },
+  {
+    cardId: "04",
+    image: "img/hedgehog.svg"
+  },
+  {
+    cardId: "05",
+    image: "img/pumpkin.svg"
+  },
+  {
+    cardId: "06",
+    image: "img/squirrel.svg"
+  },
 ]
 
-function createPair({cardId, image}) {
-    return (
-   `   <div class="scene scene--card">
+function createPair({ cardId, image }) {
+  return (
+    `   <div class="scene scene--card">
     <div id="${cardId}" class="card">
         <div class="card__face card__face--front">
         </div>
@@ -40,48 +40,49 @@ function createPair({cardId, image}) {
 };
 
 partPair.push(...partPair);
-partPair.sort(function() { return 0.5 - Math.random() });
+partPair.sort(function () { return 0.5 - Math.random() });
 
 game.innerHTML = partPair.map(createPair).join('');
 
 const cards = document.querySelectorAll('.card');
+let foundCards = 0;
 
-[...cards].forEach((card)=>{
-  card.addEventListener( 'click', () => {
+[...cards].forEach((card) => {
+  card.addEventListener('click', () => {
     card.classList.toggle('is-flipped');
     checkCards();
   });
 });
 
+
 const checkCards = () => {
-  const flippedCards = document.querySelectorAll('.is-flipped')
+  const flippedCards = document.querySelectorAll('.is-flipped');
 
-        
-     if (flippedCards.length === 2) {
-    
-      if (
-        flippedCards[0].getAttribute("id") ===
-        flippedCards[1].getAttribute("id")
-        
-      ){
-       console.log(flippedCards.length);
-        console.log("match");
-        flippedCards.forEach((card)=>{
-          card.classList.replace('is-flipped', 'found-pair');
-        });
+  if (flippedCards.length === 2) {
 
-      }else{
-        console.log("wrong");
+    if (
+      flippedCards[0].getAttribute("id") ===
+      flippedCards[1].getAttribute("id")
 
-       console.log(flippedCards.length);
-       setTimeout(() => 
-        [...cards].forEach((card)=>{
+    ) {
+      flippedCards.forEach((card) => {
+        card.classList.replace('is-flipped', 'found-pair');
+        foundCards++;
+      });
+
+    } else {
+      setTimeout(() =>
+        [...cards].forEach((card) => {
           card.classList.remove('is-flipped');
         })
-       ,500)
-      }
-    }else{
-      
-      console.log("over 3");
-    } 
+        , 500)
+    }
+  }
+  if (foundCards === 12) {
+    setTimeout(() => document.getElementById("title").innerHTML = 'You win!!!', 1500);
+    setTimeout(() => document.getElementById("conteiner").classList.toggle('conteiner-leaf-fall'), 2500)
+    setTimeout(() => document.getElementById("glass").classList.toggle('leaf-fall'), 3500);
+
+
+  }
 }
