a42019d5313f2056a08f4b2f55f293741a5ebd0c
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-09-27 11:09:11 +0300

    redesing

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
 const partPair = [
     {
-        id: "01",
+        cardId: "01",
         image: "img/acorn.svg"
      },
     {
-        id: "02",
+        cardId: "02",
         image: "img/autumn-tree.svg"
   },   
     {
-        id: "03",
+        cardId: "03",
         image: "img/dry-leaf.svg"
      },
     {
-        id: "04",
+        cardId: "04",
         image: "img/hedgehog.svg"
   }, 
     {
-        id: "05",
+        cardId: "05",
         image: "img/pumpkin.svg"
      },
     {
-        id: "06",
+        cardId: "06",
         image: "img/squirrel.svg"
   },  
 ]
 
-function createPair({id, image}) {
+function createPair({cardId, image}) {
     return (
- 
-  `   <div class="scene scene--card">
-    <div class="card">
+   `   <div class="scene scene--card">
+    <div id="${cardId}" class="card">
         <div class="card__face card__face--front">
-            <img class="card__img" src="img/card-back.svg" alt="Green leaf">
         </div>
         <div class="card__face card__face--back">
-            <img class="card__img"  src="${image}" alt="Dry leaf">
+            <img class="card__img"  src="${image}" alt="">
         </div>
     </div>
   </div> `
- 
-    
-   
-    );
+  );
 };
 
 partPair.push(...partPair);
@@ -49,10 +44,46 @@ partPair.sort(function() { return 0.5 - Math.random() });
 
 game.innerHTML = partPair.map(createPair).join('');
 
-var cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card');
 
 [...cards].forEach((card)=>{
-  card.addEventListener( 'click', function() {
+  card.addEventListener( 'click', (e) => {
     card.classList.toggle('is-flipped');
+    checkCards(e);
   });
 });
+
+const checkCards = (e) => {
+
+  console.log(e);
+  const clickedCard = e.target;
+  console.log(clickedCard);
+
+
+/*   var flippedCards = document.querySelectorAll(".is-flipped");
+  
+     if (flippedCards.length === 2) {
+    
+      if (
+        flippedCards[0].getAttribute("id") ===
+        flippedCards[1].getAttribute("id")
+        
+      ){
+       
+        console.log("match");
+      }else{        
+
+        console.log("wrong");
+        [...cards].forEach((card) => {
+          setTimeout(() => card.classList.remove('is-flipped'), 1000);
+             });
+        flippedCards = 0;
+      }
+    }else{
+      
+      
+        
+      console.log("over 3");
+    } */
+
+}
